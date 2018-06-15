dd5d08a77266cb7c3b085bfb8c33ac2d5c503be6
THIAGONOMA <thiagonoma@hotmail.com>
2018-06-14 21:17:10 -0300

    Aula 7

--- http.js
+++ http.js
@@ -1,2 +1,40 @@
-var http = require('http');
-http.createServer().listen(3412);
+var router = require('./router');
+var app = router(3412);
+
+var operadoras = [
+    {nome: "OI", codigo: 14, categoria:"Celular", preco:2},
+    {nome: "VIVO", codigo: 15, categoria:"Celular", preco:1},
+    {nome: "TIM", codigo: 41, categoria:"Celular", preco:3}
+];
+
+var contatos = [
+    {id: 1, nome:"Leticia", telefone:"9999-2222", data: new Date(), operadora: operadoras[0]},  
+    {id: 2, nome:"Thiago", telefone:"9999-3333", data: new Date(), operadora: operadoras[1]},
+    {id: 3, nome:"NinaNino", telefone:"9999-4444", data: new Date(), operadora: operadoras[2]}
+];
+
+app.get('/operadoras',function(req,res){
+    res.write(JSON.stringify(operadoras));
+    res.end();
+});
+
+
+app.get('/contatos',function(req,res){
+    res.write(JSON.stringify(contatos));
+    res.end();
+});
+
+app.post('/contatos',function(req,res){
+res.end();
+});
+
+
+/*
+http.createServer(function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    if(req.url === '/operadoras') res.write(JSON.stringify(operadoras));
+    if(req.url === '/contatos') res.write(JSON.stringify(contatos));
+    if(req.method==='GET')
+    res.end();
+}).listen(3412);
+*/
--- page.js
+++ page.js
@@ -11,4 +11,5 @@ http.createServer(function(req,res){
             '</body>'+
         '</html>'
     );
+    res.end();
 }).listen(3412);
